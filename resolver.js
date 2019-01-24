3c925ca65c57314a10a777f70121adca2efc7cbd
Oscar <oscar.a_gh@hotmail.com>
2019-01-24 11:29:41 -0600

    added resolver for findById,create,update and delete on model product

--- resolver.js
+++ resolver.js
@@ -1,15 +1,39 @@
 import Product from './models/product';
-export const resolvers ={
+import product from './models/product';
+export const resolvers = {
     Query: {
-        async allProducts(){
-            try {
-                return await Product.find();
-            } catch (error) {
-                return error;
-            }
-            
-            
-            
+        async getProduct(root, {
+            _id
+        }) {
+            return await Product.findById(_id);
+        },
+        async allProducts() {
+            return await Product.find();
+        }
+    },
+    Mutation: {
+        async createProduct(root, {
+            input
+        }) {
+            return await Product.create(input)
+        },
+        async updateProduct(root,{
+            _id,
+            input
+        }){
+            return await Product.findOneAndUpdate({
+                _id
+            },input,{
+                new:true
+            })
+        },
+        async deleteProduct(root,{
+            _id
+        }){
+            return await Product.findOneAndRemove({
+                _id
+            });
         }
     }
+
 }
